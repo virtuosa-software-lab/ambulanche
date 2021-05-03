5ded777aa010924335002458cc756deb9b5fabb0
Lana Schuster <lanaschuster1@gmail.com>
2021-05-03 20:48:42 -0300

    setup unit test

---
+++ front/src/components/forms/CategoryForm.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1 v-if="adding">Nova categoria</h1>
+    <h1 v-else>Componente de categoria</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['adding']
+}
+</script>
