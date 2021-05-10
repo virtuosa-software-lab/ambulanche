329455344a0d145dc710f8f88c182ac3730949ca
Lana Schuster <lanaschuster1@gmail.com>
2021-05-10 15:18:09 -0300

    ajuste consulta produtos: findByRestauranteId

--- src/entities/controllers/cardapio/findByRestauranteId.js
+++ src/entities/controllers/cardapio/findByRestauranteId.js
@@ -3,7 +3,18 @@ const db = require('../../../infrastructure/database/index')
 module.exports = (res, id) => {
   db.cardapio.findAll({ where: { restauranteId: id }})
     .then( response => {
-      res.status( 200 ).json( response )
+      let produtos = []
+      response.forEach(c => {
+        db.produto.findOne({ where: { id: c.produtoId} })
+          .then(r => {
+            produtos.push(r)
+          })
+          .catch(error => {
+            res.status( 400 ).send( error )
+          })
+      })
+
+      res.status( 200 ).json( produtos )
     } )
     .catch( error => {
       res.status( 400 ).send( error )
