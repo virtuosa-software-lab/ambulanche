ae060ce4253c54f2df08aff40675fdb3853432be
Lana Schuster <lanaschuster1@gmail.com>
2021-05-09 22:14:43 -0300

    ref restaurante findById

--- src/entities/controllers/restaurante/index.js
+++ src/entities/controllers/restaurante/index.js
@@ -1,11 +1,16 @@
 const Router = require('express')
 const list = require('./list')
+const findById = require('./findById')
 const create = require('./create')
 const update = require('./update')
 const restauranteRouter = Router()
 
 restauranteRouter.get('/', (req, res) => {
   list(req, res)
+})
+
+restauranteRouter.get('/:id', (req, res) => {
+  findById(res, req.params.id)
 })
 
 restauranteRouter.post('/', (req, res) => {
