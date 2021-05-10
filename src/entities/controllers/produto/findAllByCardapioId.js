22a9133a71074a762151a4289c1e6456781feccf
Luiz <lualpha_foz@hotmail.com>
2021-05-10 15:35:20 -0300

    correcao busca produtos

--- src/entities/controllers/produto/findAllByCardapioId.js
+++ src/entities/controllers/produto/findAllByCardapioId.js
@@ -2,19 +2,20 @@ const db = require('../../../infrastructure/database/index')
 
 module.exports = (res, id) => {
   db.cardapio_produto.findAll({ where: { cardapioId: id }})
-    .then( response => {
+    .then( async response => {
 
       let produtos = []
-      response.forEach(c => {
-        db.produto.findOne({ where: { id: c.produtoId } })
+      for (let i=0; i<response.length; ++i) {
+        let c = response[i]
+        await db.produto.findOne({ where: { id: c.produtoId } })
           .then(r => {
             produtos.push(r)
           })
           .catch(error => {
             res.status( 400 ).send( error )
           })
-      })
-
+      }
+      
       res.status( 200 ).json( produtos )
     } )
     .catch( error => {
